a0ef78b1b1cae09e7690cac63f50943068ac307c
MatheusGerhard <matheusmgerhard@gmail.com>
2026-03-26 20:12:39 -0300

    Lê o arquivo e diz quantas linhas tem

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 typedef struct registro {
+    struct registro *dir;
+    struct registro *esq;
+    struct registro *meio;
     int index;
     char nome[50];
     float valor;
+
 } Registro;
 
 
@@ -11,16 +15,30 @@ Registro pegaRegistro(int index) {
     return registro;
 };
 
-int percorreLista(char *nome) {
+int retornaQtd(char *nome) {
     FILE *file = fopen(nome, "r");
 
+    int qtd = 0;
+    char c;
+    qtd++;
+    while (!feof(file)) {
+        c = fgetc(file);
+        printf("%c", c);
+        if (c == '\n') {
+            qtd++;
+
+        }
+
+    }
+    return qtd;
 }
 
 
 
 int main(void) {
     const char filename[50] = "input.txt";
-
+    int valor = retornaQtd(filename);
+    printf("\nRegistros: %d\n", valor);
 
 
 
