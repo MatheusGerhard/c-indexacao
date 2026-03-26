81aacbfd2ae4a5b7d18e81b60c72c8de7d3ba79e
MatheusGerhard <matheusmgerhard@gmail.com>
2026-03-26 19:39:16 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+typedef struct registro {
+    int index;
+    char nome[50];
+    float valor;
+} Registro;
+
+
+Registro pegaRegistro(int index) {
+    Registro registro;
+    return registro;
+};
+
+int percorreLista(char *nome) {
+    FILE *file = fopen(nome, "r");
+
+}
+
+
+
+int main(void) {
+    const char filename[50] = "input.txt";
+
+
+
+
+
+    return 0;
+}
